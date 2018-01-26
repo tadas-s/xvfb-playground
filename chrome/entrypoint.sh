2ae6602b9bca66c07bfa94d5c5dabae285e6572c
Tadas Sasnauskas <tadas@yoyo.lt>
2018-01-26 15:56:07 +0000

    Run chrome on start, just to play with it

---
+++ chrome/entrypoint.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+google-chrome --window-position=0,0 --window-size=1600,1200 --test-type "about:blank"
--- xvfb/entrypoint.sh
+++ xvfb/entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-#xvfb-run -l -n 0 -s '-ac -screen 0 1024x768x24' x11vnc -v -forever
-Xvfb -listen tcp -ac :0 -screen 0 1024x768x24 &
+Xvfb -listen tcp -ac :0 -screen 0 1600x1200x24 &
 export DISPLAY=:0
 x11vnc -v -forever
